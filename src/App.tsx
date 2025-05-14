7b770828df34df4da32173fd6d1d9dc742eb383e
rasbin <rasbingodarthapa@gmail.com>
2025-05-14 17:58:09 +0545

    trying to add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-        React Banking
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </>
   );
 }
 
