e774c1139ae4a777a91b5b7e201d1b9fc30a021c
rgt <rasbin@outlook.com>
2025-05-17 22:47:06 +0545

    added login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { axiosInstance } from '../components/AxiosAPI';
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  function submitForm(e: React.FormEvent<HTMLFormElement>){
+    e.preventDefault();
+    let postBody = {
+      "email": email,
+      "password": password
+    }
+
+    axiosInstance.post("/login", postBody)
+    .then((response)=>{
+      console.log(response)
+    })
+  }
   return (
-    <div>Login</div>
+    <div>
+        <h1> Login </h1>
+        <form onSubmit={submitForm}>
+          <label htmlFor='email'> Email </label>
+          <input type="text" id="email" onChange={e=>{
+            setEmail(e.target.value)
+          }}/>
+          <label htmlFor='password'> Password</label>
+          <input type="password" id="password" onChange={e=>{
+            setPassword(e.target.value)
+          }}/>
+          <input type="submit" value="Login"/>
+        </form>
+    </div>
   )
 }
 
