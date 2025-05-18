f93e2a8494cd846c2f4547d496066fa5ed7bdc35
rgt <rasbin@outlook.com>
2025-05-18 23:58:30 +0545

    added login feature

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,21 @@
 import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./NavBar.scss";
 
 const NavBar = () => {
-  return (
-    <nav>
-      <ul className="navLinks">
-        <li>
-          <NavLink to="/" className="navLink"> Home </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" className="navLink"> Login </NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
+    const nav = useNavigate();
+    return (
+        <nav>
+            <ul className="navLinks">
+                <li>
+                    <NavLink to="/" className="navLink"> Home </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/login" className="navLink"> Login </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default NavBar;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,38 +1,67 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { axiosInstance } from '../components/AxiosAPI';
+import { AxiosResponse } from 'axios';
+
+import "../styles/Login.scss";
+
 
 const Login = () => {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+    const nav = useNavigate();
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [loginError, setLoginError] = useState("")
 
-  function submitForm(e: React.FormEvent<HTMLFormElement>){
-    e.preventDefault();
-    let postBody = {
-      "email": email,
-      "password": password
+    function submitForm(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        let postBody = {
+            "email": email,
+            "password": password
+        };
+
+        axiosInstance.post("/login", postBody)
+            .then((response: AxiosResponse) => {
+                if (response.status === 200) {
+                    console.log(response)
+                    localStorage.setItem("token", response.data.token)
+                    nav("/")
+                }
+
+            })
+            .catch((err) => {
+                if (err.response && err.response.status === 400) {
+                    setPassword("")
+                    setEmail("")
+                    setLoginError("Invalid Email or password")
+                } else {
+                    setLoginError("An unexpected error occured")
+                    console.log(`Error: ${err}`)
+                }
+            })
     }
 
-    axiosInstance.post("/login", postBody)
-    .then((response)=>{
-      console.log(response)
-    })
-  }
-  return (
-    <div>
-        <h1> Login </h1>
-        <form onSubmit={submitForm}>
-          <label htmlFor='email'> Email </label>
-          <input type="text" id="email" onChange={e=>{
-            setEmail(e.target.value)
-          }}/>
-          <label htmlFor='password'> Password</label>
-          <input type="password" id="password" onChange={e=>{
-            setPassword(e.target.value)
-          }}/>
-          <input type="submit" value="Login"/>
-        </form>
-    </div>
-  )
+    return (
+        <div className="loginContainer">
+            <h1> Login </h1>
+            <form onSubmit={submitForm} className="loginForm">
+                <div className="input">
+                    <label htmlFor='email'> Email </label>
+                    <input type="text" id="email" onChange={e => {
+                        setEmail(e.target.value)
+                    }} value={email} />
+                </div>
+                <div className='input'>
+                    <label htmlFor='password'> Password</label>
+                    <input type="password" id="password" onChange={e => {
+                        setPassword(e.target.value)
+                    }} value={password} />
+                </div>
+                <input type="submit" value="Login" className="input submit" />
+                {loginError && <p className="loginError">{loginError}</p>}
+            </form>
+        </div>
+    )
 }
 
 export default Login
